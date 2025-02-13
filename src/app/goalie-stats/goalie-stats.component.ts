65c107e3f91ced997c0182919e0653e702313c4e
Kalle Haavisto <kalle.haavisto@geniem.com>
2025-02-13 22:35:53 +0200

    add per game support to goalie view as well

--- src/app/goalie-stats/goalie-stats.component.ts
+++ src/app/goalie-stats/goalie-stats.component.ts
@@ -7,6 +7,7 @@ import {
   ReportType,
 } from '@services/api.service';
 import { FilterService } from '@services/filter.service';
+import { StatsService } from '@services/stats.service';
 import { ControlPanelComponent } from '@shared/control-panel/control-panel.component';
 import { StatsTableComponent } from '@shared/stats-table/stats-table.component';
 import { GOALIE_COLUMNS, GOALIE_SEASON_COLUMNS } from '@shared/table-columns';
@@ -20,10 +21,12 @@ import { GOALIE_COLUMNS, GOALIE_SEASON_COLUMNS } from '@shared/table-columns';
 export class GoalieStatsComponent implements OnInit, OnDestroy {
   private apiService = inject(ApiService);
   private filterService = inject(FilterService);
+  private statsService = inject(StatsService);
   private subscriptions = new Subscription();
 
   reportType: ReportType = 'regular';
   season?: number;
+  statsPerGame: boolean = false;
   tableData: Goalie[] = [];
   tableColumns = GOALIE_COLUMNS;
 
@@ -39,6 +42,12 @@ export class GoalieStatsComponent implements OnInit, OnDestroy {
     this.subscriptions.add(
       this.filterService.season$.subscribe((season) => {
         this.changeSeason(season);
+      })
+    );
+
+    this.subscriptions.add(
+      this.filterService.statsPerGame$.subscribe((statsPerGame) => {
+        this.toggleStatsMode(statsPerGame);
       })
     );
   }
@@ -61,9 +70,16 @@ export class GoalieStatsComponent implements OnInit, OnDestroy {
     this.fetchData({ reportType: this.reportType, season });
   }
 
+  toggleStatsMode(statsPerGame: boolean) {
+    this.statsPerGame = statsPerGame;
+    this.fetchData({ reportType: this.reportType, season: this.season });
+  }
+
   fetchData(params: ApiParams = {}) {
     this.apiService.getGoalieData(params).subscribe((data) => {
-      this.tableData = data;
+      this.tableData = this.statsPerGame
+        ? this.statsService.getGoalieStatsPerGame(data)
+        : data;
     });
   }
 }
