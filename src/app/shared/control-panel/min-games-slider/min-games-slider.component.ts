b39aae1dc30022a2486ce78e2b3b572a649b918f
Kalle Haavisto <kalle.haavisto@geniem.com>
2025-11-08 19:47:58 +0200

    Fix situation when minGames can be more than maxGames

--- src/app/shared/control-panel/min-games-slider/min-games-slider.component.ts
+++ src/app/shared/control-panel/min-games-slider/min-games-slider.component.ts
@@ -1,4 +1,12 @@
-import { Component, Input, OnInit, OnDestroy, inject } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnDestroy,
+  OnChanges,
+  SimpleChanges,
+  inject,
+} from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { Subject, takeUntil } from 'rxjs';
@@ -10,11 +18,11 @@ import { FilterService, FilterState } from '@services/filter.service';
   templateUrl: './min-games-slider.component.html',
   styleUrl: './min-games-slider.component.scss',
 })
-export class MinGamesSliderComponent implements OnInit, OnDestroy {
+export class MinGamesSliderComponent implements OnInit, OnDestroy, OnChanges {
   @Input() context: 'player' | 'goalie' = 'player';
   @Input() maxGames = 0;
 
-  minGames = 0;
+  minGames: number = 0;
   private filterService = inject(FilterService);
   private destroy$ = new Subject<void>();
 
@@ -28,6 +36,15 @@ export class MinGamesSliderComponent implements OnInit, OnDestroy {
       .subscribe((filters: FilterState) => {
         this.minGames = filters.minGames;
       });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    // We need to update minGames if maxGames decreases below it
+    if (changes['maxGames'] && this.maxGames < this.minGames) {
+      this.minGames = this.maxGames;
+      // Defer this so Angular can finish the change detection cycle
+      Promise.resolve().then(() => this.onValueChange(this.minGames));
+    }
   }
 
   onValueChange(minGames: number) {
